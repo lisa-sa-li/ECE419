83925ad55ad68e7fb16fd94d66f8ced67addaf1b
akino11111 <akino.wata@gmail.com>
2022-01-27 13:59:53 -0500

    update files performance test

--- Milestone_1/src/testing/PerformanceTest.java
+++ Milestone_1/src/testing/PerformanceTest.java
@@ -2,22 +2,31 @@ package testing;
 
 import client.KVStore;
 
-
 public class PerformanceTest {
 
-    private KVServer server;
+    private KVStore kvStore;
     private static final int numRequests = 2000;
 
     public PerformanceTest() {
-
+        try {
+            kvStore = new KVStore("localhost", 50000);
+        } catch (Exception e) {
+            System.err.println(e.message());
+        }
     }
 
-    public void testStub() {
-        assertTrue(true);
+    public void runTest() {
+        // Iterate to test different ratios of PUT and GET requests to the server
+        float[] ratioListPUT = [0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2];
+        for (int i = 0; i < ratioListPUT.length; i++) {
+            for (int j = 0; j < numRequests; j++) {
+                
+            }
+        }
     }
 
     public static void main(String[] args)  {
-        new PerformanceTest().runTest();
+        new PerformanceTest().runTests();
     }
 
 }
