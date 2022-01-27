ffde13763e4d1fa8d12e0fdfab6063ff8f0cb880
akino11111 <akino.wata@gmail.com>
2022-01-27 00:16:19 -0500

    commit before pull

--- Milestone_1/src/shared/messages/KVMessage.java
+++ Milestone_1/src/shared/messages/KVMessage.java
@@ -11,7 +11,7 @@ public interface KVMessage {
 		PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
 		PUT_ERROR, 		/* Put - request not successful */
 		DELETE_SUCCESS, /* Delete - request successful */
-		DELETE_ERROR 	/* Delete - request successful */
+		DELETE_ERROR 	/* Delete - request not successful */
 	}
 
 	/**
