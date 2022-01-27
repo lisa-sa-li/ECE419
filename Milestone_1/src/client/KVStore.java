3cbdffa0809a779e96dbee603662638d4d4a5f4d
petksand <petksand@gmail.com>
2022-01-26 23:48:04 -0500

    Relocating receiveJSON to KVStore

--- Milestone_1/src/client/KVStore.java
+++ Milestone_1/src/client/KVStore.java
@@ -143,4 +143,76 @@ public class KVStore implements KVCommInterface {
 				+ clientSocket.getPort() + ">: '" + msg.getMsg() + "'");
 		return msg;
 	}
+
+	private JSONMessage receiveJSONMessage() throws IOException {
+		
+		int index = 0;
+		byte[] msgBytes = null, tmp = null;
+		byte[] bufferBytes = new byte[BUFFER_SIZE];
+		
+		/* read first char from stream */
+		byte read = (byte) input.read();	
+		boolean reading = true;
+		
+//		logger.info("First Char: " + read);
+//		Check if stream is closed (read returns -1)
+//		if (read == -1){
+//			TextMessage msg = new TextMessage("");
+//			return msg;
+//		}
+
+		while(/*read != 13  && */ read != 10 && read !=-1 && reading) {/* CR, LF, error */
+			/* if buffer filled, copy to msg array */
+			if(index == BUFFER_SIZE) {
+				if(msgBytes == null){
+					tmp = new byte[BUFFER_SIZE];
+					System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
+				} else {
+					tmp = new byte[msgBytes.length + BUFFER_SIZE];
+					System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+					System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
+							BUFFER_SIZE);
+				}
+
+				msgBytes = tmp;
+				bufferBytes = new byte[BUFFER_SIZE];
+				index = 0;
+			} 
+			
+			/* only read valid characters, i.e. letters and constants */
+			bufferBytes[index] = read;
+			index++;
+			
+			/* stop reading is DROP_SIZE is reached */
+			if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+				reading = false;
+			}
+			
+			/* read next char from stream */
+			read = (byte) input.read();
+		}
+		
+		if(msgBytes == null){
+			tmp = new byte[index];
+			System.arraycopy(bufferBytes, 0, tmp, 0, index);
+		} else {
+			tmp = new byte[msgBytes.length + index];
+			System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+			System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
+		}
+		
+		msgBytes = tmp;
+		
+		/* build final Object */
+		JSONMessage json = new JSONMessage();
+		// bytes to string
+		String jsonStr = json.byteToString(tmp);
+		// deserialize
+		json.deserialize(jsonStr);
+		logger.info("RECEIVE \t<" 
+				+ clientSocket.getInetAddress().getHostAddress() + ":" 
+				+ clientSocket.getPort() + ">: '" 
+				+ json.getJSON().trim() + "'");
+		return json;
+    }
 }
