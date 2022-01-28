510e6b6c0dc9c7daa61c85ec3d96adc0dea6e1c5
akino11111 <akino.wata@gmail.com>
2022-01-27 21:34:36 -0500

    performance test

--- Milestone_1/src/testing/PerformanceTest.java
+++ Milestone_1/src/testing/PerformanceTest.java
@@ -2,27 +2,29 @@ package testing;
 
 import app_kvServer.KVServer;
 import shared.messages.KVMessage.StatusType;
+import java.util.Random;
+import java.lang.Math;
 
 public class PerformanceTest {
 
     private KVServer kvServer;
     private static final int numRequests = 2000;
-    private long totalDurationPUT = 0;
-    private long totalDurationGET = 0;
-    private int numPUTInsertRequests = 0;
-    private int numPUTUpdateRequests = 0;
-    private int numGETRequests = 0;
+    double totalDurationPUT;
+    double totalDurationGET;
+    int numPUTRequests;
+    int numGETRequests;
+    Random randomNumber = new Random();
 
 
     public PerformanceTest() {
         try {
-            this.kvServer = new KVServer(8080, 0, "");
+            this.kvServer = new KVServer(8082, 0, "");
         } catch (Exception e) {
             System.out.println("Couldn't connect to server. " + e);
         }
     }
 
-    public void computeDurationPUT(String key, String val) {
+    public double computeTimeDurationPUT(String key, String val) {
         long startTime = System.currentTimeMillis();
         try {
             StatusType outputStatus = this.kvServer.putKV(key, val);
@@ -30,15 +32,11 @@ public class PerformanceTest {
             System.out.println("Error when doing PUT request. " + e);
         }
         long endTime = System.currentTimeMillis();
-        totalDurationPUT += (endTime - startTime) / 1000;
-        if (val != "null") {
-            numPUTInsertRequests++;
-        } else {
-            numPUTUpdateRequests++;
-        }
+        numPUTRequests++;
+        return (endTime - startTime) / 1000;
     }
 
-    public void computeDurationGET(String key) {
+    public double computeTimeDurationGET(String key) {
         long startTime = System.currentTimeMillis();
         try {
             String getOutput = this.kvServer.getKV(key);
@@ -46,21 +44,49 @@ public class PerformanceTest {
             System.out.println("Error when doing GET request. " + e);
         }
         long endTime = System.currentTimeMillis();
-        totalDurationGET += (endTime - startTime) / 1000;
         numGETRequests++;
+        return (endTime - startTime) / 1000;
+    }
+
+    public String generateKey() {
+        String key = "";
+        for (int i = 0; i < 10; i++) {
+            char c1 = (char)(randomNumber.nextInt(26) + 'a'); // https://stackoverflow.com/questions/2626835/is-there-functionality-to-generate-a-random-character-in-java
+            key = key + c1;
+        }
+        return key;
     }
 
     public void runTests() {
         // Iterate to test different ratios of PUT and GET requests to the server
         double[] ratioListPUT = {0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2};
         for (int i = 0; i < ratioListPUT.length; i++) {
+            double ratioPUT = ratioListPUT[i];
+            totalDurationPUT = 0;
+            totalDurationGET = 0;
+            numPUTRequests = 0;
+            numGETRequests = 0;
             for (int j = 0; j < numRequests; j++) {
-                System.out.println("IN loop");
-                
+                // Do PUT and GET request operations
+                double randomRatio = Math.random();
+                String key = generateKey();
+                if (randomRatio <= ratioPUT) {
+                    totalDurationPUT += computeTimeDurationPUT(key, "abcdefghijklmnopqrstuvwxyz");
+                } else {
+                    totalDurationGET += computeTimeDurationGET(key);
+                }
+            }
+            double percentagePUT = ratioPUT * 100.0;
+            double percentageGET = 100.0 - percentagePUT;
+            double latency = 1000.0 * (totalDurationPUT + totalDurationGET) / numRequests;
+            System.out.println("The latency of " + percentagePUT + "% PUT requests and " + percentageGET + "% GET requests is: " + latency);
+            try {
+                this.kvServer.clearStorage();
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
     }
-
 
     public static void main(String[] args) {
         new PerformanceTest().runTests();
