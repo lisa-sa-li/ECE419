9ca6828fb7bbc2d6e4f465d830ed2b69d4b9ad3d
Lisa Li <lisasiaili@gmail.com>
2022-01-27 21:32:15 -0500

    add value in a put

--- Milestone_1/src/app_kvServer/ServerConnection.java
+++ Milestone_1/src/app_kvServer/ServerConnection.java
@@ -158,7 +158,7 @@ public class ServerConnection implements IServerConnection, Runnable {
 		String value = msg.getValue();
 		StatusType status = msg.getStatus();
 
-		String handleMessageValue = "";
+		String handleMessageValue = value; // Send back the value in a PUT
 		StatusType handleMessageStatus = StatusType.NO_STATUS;
 		JSONMessage handleMessage = new JSONMessage();
 
